2bbd9775460d112396d71b6fbf4ee76a41261947
mousam <mousam@pop-os.localdomain>
2021-09-23 11:27:47 +0530

    first commit

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c98b78fca069f2eb60391c4e73571680'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2bf3d413-5520-46bb-872c-81cb0e423779'
+};
